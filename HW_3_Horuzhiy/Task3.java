3768e06ec07c2db68313181e71b1fc68fe42f1f3
Fedor <vailbi94@gmail.com>
2023-03-30 19:07:49 +0300

    add hw3

---
+++ HW_3_Horuzhiy/Task3.java
@@ -0,0 +1,38 @@
+package HW_3_Horuzhiy;
+import java.util.ArrayList;
+public class Task3 {
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>();
+        array = lib.FillRandomArray(10);
+        lib.PrintArray(array);
+        int maxElement = findMax(array);
+        int minElement = findMin(array);
+        Double avgElement = findAvg(array);
+        System.out.printf("Max value: %d \n", maxElement);
+        System.out.printf("Min value: %d \n", minElement);
+        System.out.printf("Avg value: %.1f \n", avgElement);
+
+    }
+    public static int findMax(ArrayList<Integer> arr){
+        int max = arr.get(0);
+        for(int element : arr){
+            if(element > max) max = element;
+        }
+        return max;
+    }
+    public static int findMin(ArrayList<Integer> arr){
+        int min = arr.get(0);
+        for(int element : arr){
+            if(element < min) min = element;
+        }
+        return min;
+    }
+    public static Double findAvg(ArrayList<Integer> arr){
+        int sum = 0;
+        for(int element : arr){
+            sum += element;
+        }
+        Double avg = Double.valueOf(sum)/Double.valueOf(arr.size());
+        return avg;
+    }
+}
